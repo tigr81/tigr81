ff1ad1d99a9766cc3cf66adaa87d2e0140273ad6
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-10-29 03:16:56 +0100

    test(hub/helpers): get_template_from_hubs

--- tigr81/tests/commands/hub/test_helpers.py
+++ tigr81/tests/commands/hub/test_helpers.py
@@ -1,5 +1,6 @@
 import pytest
 import typer
+from pytest_mock import MockerFixture
 
 from tigr81.commands.hub.helpers import (
     get_template_from_hubs,
@@ -39,14 +40,14 @@ Unit tests for is_hub_name_valid
 """
 
 
-def mock_load_hubs(mocker, mock_hub_data):
+def mock_load_hubs(mocker: MockerFixture, mock_hub_data):
     """Mock load hubs."""
     return mocker.patch(
         "tigr81.commands.hub.helpers.load_hubs", return_value=mock_hub_data
     )
 
 
-def test_is_hub_name_valid_with_valid_hub_name(mocker, mock_hub_folders, mock_hub_data):
+def test_is_hub_name_valid_with_valid_hub_name(mocker: MockerFixture, mock_hub_folders, mock_hub_data):
     """Test with a hub name that exists in the loaded hubs."""
     mock_load_hubs(mocker, mock_hub_data)
     hub_name = "valid_hub_1"
@@ -54,7 +55,7 @@ def test_is_hub_name_valid_with_valid_hub_name(mocker, mock_hub_folders, mock_hu
 
 
 def test_is_hub_name_valid_with_invalid_hub_name(
-    mocker, mock_hub_folders, mock_hub_data
+    mocker: MockerFixture, mock_hub_folders, mock_hub_data
 ):
     """Test with a hub name that does not exist in the loaded hubs."""
     mock_load_hubs(mocker, mock_hub_data)
@@ -62,7 +63,7 @@ def test_is_hub_name_valid_with_invalid_hub_name(
     assert is_hub_name_valid(hub_name, mock_hub_folders)
 
 
-def test_is_hub_name_valid_with_empty_hubs(mocker, mock_hub_folders):
+def test_is_hub_name_valid_with_empty_hubs(mocker: MockerFixture, mock_hub_folders):
     """Test with an empty hubs dictionary from load_hubs."""
     mock_load_hubs(mocker, {})
     hub_name = "any_hub_name"
@@ -73,11 +74,35 @@ def test_is_hub_name_valid_with_empty_hubs(mocker, mock_hub_folders):
 Unit tests for get_template_from_hubs
 """
 
+
 def test_get_template_from_hubs_no_hubs():
     """Test get template from hubs with no hubs available."""
     with pytest.raises(typer.Exit):
-        get_template_from_hubs(
-            hub_name="pippo",
-            template_name="pluto",
-            hubs={}
-        )
+        get_template_from_hubs(hub_name="pippo", template_name="pluto", hubs={})
+
+
+def test_get_template_from_hubs_none_template(mocker: MockerFixture):
+    """Test get template from hubs with an hub template not present in the hub."""
+    mock_hub = mocker.Mock()
+    mock_hub.hub_templates = {}
+
+    hubs = {"pippo": mock_hub}
+
+    with pytest.raises(typer.Exit):
+        get_template_from_hubs(hub_name="pippo", template_name="pluto", hubs=hubs)
+
+
+
+def test_get_template_from_hubs_hub_template_found(mocker: MockerFixture):
+    """Test get template from hubs with an hub template present in the hub."""
+    mock_hub = mocker.Mock()
+    mock_hub.hub_templates = {
+        "pluto": "pluto"
+    }
+
+    hubs = {"pippo": mock_hub}
+
+
+    hub_template = get_template_from_hubs(hub_name="pippo", template_name="pluto", hubs=hubs)
+
+    assert hub_template is not None
